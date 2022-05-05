07a1ba9c475714f3720b9b840007467e20546e7c
Furfreiheit <websvc.yy8018@gmail.com>
2022-05-05 19:57:40 +0900

    firebase step up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
-
+import React from 'react';
+// import {
+//   auth,
+//   db,
+//   loginWithEmailAndPassword,
+//   logInWithGoogle,
+//   logout,
+//   registerWithEmailAndPassword,
+// } from "./firebase";
 function App() {
   return (
     <div className="App">
